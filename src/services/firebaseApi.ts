149c5707fae4cda157c2982602728641f9507fdd
Artem Bazyl <a4b11s@gmail.com>
2023-04-12 20:27:19 +0300

    feat!: add firebase api

---
+++ src/services/firebaseApi.ts
@@ -0,0 +1,29 @@
+import { onValue, ref, set, update } from 'firebase/database';
+
+import { DataBase } from './firebase';
+
+export const subscribeBD = (
+	path: string,
+	callback: (data: any) => void,
+	dataMappingFunction: (data: any) => any
+) => {
+	return onValue(ref(DataBase, path), (snapshot) => {
+		const data = snapshot.val();
+
+		if (!data) {
+			callback(null);
+			return;
+		}
+
+		const mappedData = dataMappingFunction(data);
+		callback(mappedData);
+	});
+};
+
+export const sendToDataBase = (path: string, data: any) => {
+	set(ref(DataBase, path), data);
+};
+
+export const sendTransaction = (updates: {}) => {
+	return update(ref(DataBase), updates);
+};
