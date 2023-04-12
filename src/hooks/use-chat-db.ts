97d1a3ec756a1c4d80836be3e393827cd398edb4
Artem Bazyl <a4b11s@gmail.com>
2023-04-12 20:27:44 +0300

    feat!: add chat db hooks

---
+++ src/hooks/use-chat-db.ts
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+
+import { sendToDataBase, subscribeBD } from '../services/firebaseApi';
+import { IChat } from '../models';
+
+const defaultMappingDataFunction = (data: any) => {
+	return Object.keys(data).map((item) => {
+		return data[item as any];
+	});
+};
+
+export const useSubChats = (
+	callback: (data: any) => void,
+	mappingDataFunction: (data: any) => typeof data = defaultMappingDataFunction
+) => {
+	useEffect(
+		() => {
+			return subscribeBD('chats/', callback, mappingDataFunction);
+		}, // eslint-disable-next-line react-hooks/exhaustive-deps
+		[]
+	);
+};
+
+export const addNewChat = (newChat: any) => {
+	const unqChatId = Date.now().toString();
+	const chat: IChat = {
+		uid: unqChatId,
+		lastMessage: 'empty',
+		...newChat,
+	};
+	sendToDataBase(`chats/${unqChatId}`, chat);
+};
+export const useChatsDataBase = () => {
+	return {
+		useSubChats,
+		addNewChat,
+	};
+};
