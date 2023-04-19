1e12dd3d17e0938a3bb6c9f4b846e926071838e0
Artem Bazyl <a4b11s@gmail.com>
2023-04-19 20:12:01 +0300

    feat!: add new hook useAuth

---
+++ src/hooks/use-auth.ts
@@ -0,0 +1,33 @@
+import { IUser } from '../models';
+import {
+	ICredential,
+	loginWithCredential,
+	loginWithGoogle,
+	signInWithCredential,
+} from '../services/authApi';
+
+export const useAuth = () => {
+	const auth = (
+		cb: (user: IUser) => void,
+		credential?: ICredential,
+		reject: (error: string) => void = () => {}
+	) => {
+		if (credential) {
+			loginWithCredential(credential, cb, reject);
+		} else {
+			loginWithGoogle(cb, reject);
+		}
+	};
+	const signIn = (
+		cb: (user: IUser) => void,
+		credential: ICredential,
+		reject: (error: string) => void = () => {}
+	) => {
+		signInWithCredential(credential, cb, reject);
+	};
+
+	return {
+		signIn,
+		auth,
+	};
+};
