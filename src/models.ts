1aadde30ed37c201cb9aeb3b829ab80ea9c4de60
Artem Bazyl <a4b11s@gmail.com>
2023-04-19 22:16:54 +0300

    feat!: add new file models.ts

---
+++ src/models.ts
@@ -0,0 +1,22 @@
+export interface IUser {
+	displayName: string;
+	email: string;
+	uid: string;
+	photoURL: string;
+}
+
+export interface IChat {
+	lastMessage: string;
+	uid: string;
+	name: string;
+	chatAvatar: string;
+}
+
+export interface IMessage {
+	chatUid: string;
+	uid: string;
+	author: {
+		uid: string;
+	};
+	text: string;
+}
