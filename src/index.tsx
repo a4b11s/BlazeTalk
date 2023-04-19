6bbb9837311556129ff9eec6e2bea829376df26f
Artem Bazyl <a4b11s@gmail.com>
2023-04-19 22:19:17 +0300

    feat!: add theme, ThemeProvider, store provider and BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
 
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
-import '@fontsource/montserrat';
-import 'normalize.css'
+import '@fontsource/roboto';
+import 'normalize.css';
 
 import './index.css';
 import App from './App';
+import store from './store/store';
+
+import { createTheme, ThemeProvider } from '@mui/material';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById('root') as HTMLElement
 );
+
+const theme = createTheme({
+	palette: {
+		primary: { main: '#067988', light: '#37939f', contrastText: '#ffffff' },
+		secondary: {
+			main: '#df5026',
+			light: '#e57351',
+			contrastText: '#fef95a',
+		},
+	},
+});
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<BrowserRouter>
+		<Provider store={store}>
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
+		</Provider>
+	</BrowserRouter>
 );
