b725155317c8efdde442ac4437b1142ecf9474ea
Artem Bazyl <a4b11s@gmail.com>
2023-04-21 18:01:02 +0300

    feat!: add new component "App"

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
 
-function App() {
-  return (
-   <div>
+import Layout from './Layout';
+import ChatPage from './view/ChatPage';
+import { useUser } from './hooks/use-user';
+import Auth from './components/Auth/Auth';
 
-   </div>
-  );
+function App() {
+	const { isAuth } = useUser();
+
+	useEffect(() => {}, []);
+	return (
+		<Routes>
+			{isAuth ? (
+				<Route path="/" element={<Layout />}>
+					<Route index={true} path="/:chatId" element={<ChatPage />} />
+				</Route>
+			) : (
+				<Route path="*" element={<Auth />} />
+			)}
+		</Routes>
+	);
 }
 
 export default App;
