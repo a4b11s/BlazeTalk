622eff37c301cedc6dba0d8ee51659648e81ad5e
Artem Bazyl <a4b11s@gmail.com>
2023-04-21 23:30:37 +0300

    feat: add uploading avatar

--- src/components/AddNewChat/AddNewChat.tsx
+++ src/components/AddNewChat/AddNewChat.tsx
@@ -1,101 +1,120 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-import {Avatar, Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Slide, TextField,} from '@mui/material';
-import {TransitionProps} from '@mui/material/transitions';
+import FileInput from '../FileInput/FileInput';
+import { uploadFile } from '../../services/firebaseStorageApi';
+
+import {
+	Avatar,
+	Box,
+	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogTitle,
+	Slide,
+	TextField,
+} from '@mui/material';
+import { TransitionProps } from '@mui/material/transitions';
 
 interface IProps {
-    isAddingNewChat: boolean;
-    onAddingModalClose: () => void;
-    onAddComplete: (newChat: { name: string; chatAvatar: string }) => void;
+	isAddingNewChat: boolean;
+	onAddingModalClose: () => void;
+	onAddComplete: (newChat: { name: string; chatAvatar: string }) => void;
 }
 
 const Transition = React.forwardRef(function Transition(
-    props: TransitionProps & {
-        children: React.ReactElement<any, any>;
-    },
-    ref: React.Ref<unknown>
+	props: TransitionProps & {
+		children: React.ReactElement<any, any>;
+	},
+	ref: React.Ref<unknown>
 ) {
-    return <Slide direction="up" ref={ref} {...props} />;
+	return <Slide direction="up" ref={ref} {...props} />;
 });
 
 const AddNewChat = (props: IProps) => {
-    const {isAddingNewChat, onAddingModalClose, onAddComplete} = props;
-    const [chatName, setChatName] = useState('');
-    const [avatarUrl, setAvatarUrl] = useState('');
+	const { isAddingNewChat, onAddingModalClose, onAddComplete } = props;
+	const [chatName, setChatName] = useState<string>('');
+	const [avatar, setAvatar] = useState<File>();
+	const [avatarUrl, setAvatarUrl] = useState<string>();
 
-    const onClickCreateBtn = () => {
-        const newChat = {name: chatName, chatAvatar: avatarUrl};
-        setChatName('');
-        setAvatarUrl('');
-        onAddComplete(newChat);
-        onAddingModalClose();
-    };
-    return (
-        <Dialog
-            data-testid="wrapper"
-            open={isAddingNewChat}
-            TransitionComponent={Transition}
-            keepMounted
-            onClose={onAddingModalClose}
-            aria-describedby="Adding new chat"
-        >
-            <DialogTitle>{'Adding new chat'}</DialogTitle>
-            <DialogContent sx={{width: '600px'}} dividers>
-                <Box
-                    display="flex"
-                    flexDirection="column"
-                    alignContent="stretch"
-                    width="100%"
-                >
-                    <TextField
-                        inputProps={{'data-testid': 'chat-name-input'}}
-                        value={chatName}
-                        onChange={(e) => {
-                            setChatName(e.target.value);
-                        }}
-                        id="Chat name"
-                        label="Chat name"
-                        type="text"
-                        variant="standard"
-                    />
-                    <Box display="flex" height="100px" alignItems="center">
-                        <Avatar src={avatarUrl} data-testid="avatar"/>
-                        <TextField
-                            inputProps={{'data-testid': 'avatar-url-input'}}
-                            value={avatarUrl}
-                            onChange={(e) => {
-                                setAvatarUrl(e.target.value);
-                            }}
-                            sx={{flexGrow: 1, ml: 1}}
-                            id="avatar-input"
-                            label="Chat avatar"
-                            type="text"
-                            variant="standard"
-                        />
-                    </Box>
-                </Box>
-            </DialogContent>
-            <DialogActions>
-                <Button
-                    data-testid="close-btn"
-                    color="error"
-                    variant="outlined"
-                    onClick={onAddingModalClose}
-                >
-                    Close
-                </Button>
-                <Button
-                    data-testid="create-btn"
-                    disabled={chatName.trim().length <= 2}
-                    color="success"
-                    variant="outlined"
-                    onClick={onClickCreateBtn}
-                >
-                    Create
-                </Button>
-            </DialogActions>
-        </Dialog>
-    );
+	const onClickCreateBtn = () => {
+		if (!avatar) return;
+		uploadFile(`/chat_avatars/${avatar.name + Date.now()}`, avatar).then(
+			(value) => {
+				const newChat = { name: chatName, chatAvatar: value.ref.name };
+				setChatName('');
+				setAvatar(undefined);
+				setAvatarUrl('');
+				onAddComplete(newChat);
+				onAddingModalClose();
+			}
+		);
+	};
+
+	const onFileUpload = (FileList: FileList) => {
+		const reader = new FileReader();
+		reader.onload = () => {
+			setAvatarUrl(reader.result as string);
+		};
+		reader.readAsDataURL(FileList[0]);
+		setAvatar(FileList[0]);
+	};
+
+	return (
+		<Dialog
+			data-testid="wrapper"
+			open={isAddingNewChat}
+			TransitionComponent={Transition}
+			keepMounted
+			onClose={onAddingModalClose}
+			aria-describedby="Adding new chat"
+		>
+			<DialogTitle>{'Adding new chat'}</DialogTitle>
+			<DialogContent sx={{ width: '600px' }} dividers>
+				<Box
+					display="flex"
+					flexDirection="column"
+					alignContent="stretch"
+					width="100%"
+				>
+					<TextField
+						inputProps={{ 'data-testid': 'chat-name-input' }}
+						value={chatName}
+						onChange={(e) => {
+							setChatName(e.target.value);
+						}}
+						id="Chat name"
+						label="Chat name"
+						type="text"
+						variant="standard"
+					/>
+					<Box display="flex" height="100px" alignItems="center">
+						<Avatar src={avatarUrl} data-testid="avatar" />
+						<FileInput onFileUploaded={onFileUpload} label="Upload avatar" />
+					</Box>
+				</Box>
+			</DialogContent>
+			<DialogActions>
+				<Button
+					data-testid="close-btn"
+					color="error"
+					variant="outlined"
+					onClick={onAddingModalClose}
+				>
+					Close
+				</Button>
+				<Button
+					data-testid="create-btn"
+					disabled={chatName.trim().length <= 2}
+					color="success"
+					variant="outlined"
+					onClick={onClickCreateBtn}
+				>
+					Create
+				</Button>
+			</DialogActions>
+		</Dialog>
+	);
 };
 
 export default AddNewChat;
