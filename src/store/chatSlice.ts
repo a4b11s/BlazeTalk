5fe34c745c4e289b4fcf3b875a88a5e1e38a32a7
Artem Bazyl <a4b11s@gmail.com>
2023-04-22 19:35:05 +0300

    feat: remove test data

--- src/store/chatSlice.ts
+++ src/store/chatSlice.ts
@@ -1,38 +1,23 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-import {IChat} from '../models';
+import { IChat } from '../models';
 
 interface IState {
-    chatList: Array<IChat>;
+	chatList: Array<IChat>;
 }
 
 const initialState: IState = {
-    chatList: [
-        {
-            uid: 'string',
-            name: 'Some chat name1',
-            chatAvatar:
-                'https://lh3.googleusercontent.com/a/AGNmyxYOwq7vS7nFuhvGGUWrRxe98Nbi2n1PAb3clXMAd2w=s83-c-mo',
-            lastMessage: 'someMessage',
-        },
-        {
-            uid: 'string2',
-            name: 'Some chat name2',
-            chatAvatar:
-                'https://lh3.googleusercontent.com/a/AGNmyxYOwq7vS7nFuhvGGUWrRxe98Nbi2n1PAb3clXMAd2w=s83-c-mo',
-            lastMessage: 'someMessage',
-        },
-    ],
+	chatList: [],
 };
 
 export const chatSlice = createSlice({
-    name: 'chat',
-    initialState,
-    reducers: {
-        setChats: (state, action) => {
-            state.chatList = action.payload;
-        },
-    },
+	name: 'chat',
+	initialState,
+	reducers: {
+		setChats: (state, action) => {
+			state.chatList = action.payload;
+		},
+	},
 });
 
-export const {setChats} = chatSlice.actions;
+export const { setChats } = chatSlice.actions;
