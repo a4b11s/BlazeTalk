187a4634c895ce816c1dba81d9cccc76a9d83f5c
Artem Bazyl <a4b11s@gmail.com>
2023-04-23 21:32:14 +0300

    refactor: extract css on file

--- src/components/ImageList/ImageList.tsx
+++ src/components/ImageList/ImageList.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import classes from './ImageList.module.css';
+
 import { Box, useTheme } from '@mui/material';
 import ClearIcon from '@mui/icons-material/Clear';
 
@@ -19,41 +21,20 @@ const ImageList = (props: IProps) => {
 
 	if (!images) return null;
 
-	const badgeSx = {
-		cursor: 'pointer',
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center',
-		color: 'white',
-		borderRadius: '100%',
-		position: 'absolute',
-		top: '-7px',
-		right: '-7px',
-		bgcolor: theme.palette.secondary.main,
-		width: '24px',
-		height: '24px',
-	};
-
 	return (
-		<Box display="flex" flexWrap="wrap" px="102px">
+		<Box className={classes.listWrapper}>
 			{images.map((img, index) => {
 				const { uid, blobUrl } = img;
 				return (
-					<Box
-						my="6px"
-						ml="12px"
-						height="100px"
-						width="fit-content"
-						position="relative"
-						key={blobUrl}
-					>
+					<Box className={classes.itemWrapper} key={blobUrl}>
 						<img height="100%" src={blobUrl} alt={`#${index}`} />
 						{onDeleteButtonClick && (
 							<Box
 								onClick={() => {
 									onDeleteButtonClick(uid);
 								}}
-								sx={badgeSx}
+								className={classes.badge}
+								sx={{ bgcolor: theme.palette.secondary.main }}
 							>
 								<ClearIcon fontSize="small" color="inherit" />
 							</Box>
