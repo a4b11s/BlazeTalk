7929a3dfd68b9a37127e29c4956b0f197ece59f9
Artem Bazyl <a4b11s@gmail.com>
2023-04-23 21:08:11 +0300

    feat: remarkup Layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
 
-import {Outlet} from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Footer from './components/Footer/Footer';
 
 const Layout = () => {
-    return (
-        <>
-            <header id="header">
-                <Header/>
-            </header>
-            <main id="main">
-                <Sidebar/>
-                <section id="content">
-                    <Outlet/>
-                </section>
-            </main>
-            <footer id="footer">
-                <Footer/>
-            </footer>
-        </>
-    );
+	return (
+		<>
+			<header id="header">
+				<Header />
+			</header>
+			<Sidebar />
+			<main id="main">
+				<Outlet />
+			</main>
+
+			<footer id="footer">
+				<Footer />
+			</footer>
+		</>
+	);
 };
 
 export default Layout;
