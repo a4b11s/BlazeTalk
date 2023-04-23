0c600cfa7757a141aa4c651602222ba640b91229
Artem Bazyl <a4b11s@gmail.com>
2023-04-23 22:42:19 +0300

    refactor: remarkup

--- src/components/MessageList/MessageList.tsx
+++ src/components/MessageList/MessageList.tsx
@@ -1,46 +1,42 @@
 import React from 'react';
 
 import Message from '../Message/Message';
-import {IMessage, IUser} from '../../models';
+import { IMessage, IUser } from '../../models';
 
-import {useUser} from '../../hooks/use-user';
+import { useUser } from '../../hooks/use-user';
 
-import {Box} from '@mui/material';
+import classes from './MessageList.module.css';
+
+import { Box } from '@mui/material';
 
 interface IProps {
-    messageArray: IMessage[];
-    users: IUser[];
+	messageArray: IMessage[] | null;
+	users: IUser[] | null;
 }
 
 const MessageList = (props: IProps) => {
-    const {messageArray, users} = props;
-    const {uid} = useUser();
+	const { messageArray, users } = props;
+	const { uid } = useUser();
 
-    const wrapperStyle = {
-        overflowY: 'scroll',
-        bgcolor: "rgba(230,251,255,0.3)",
-        display: "flex",
-        flexDirection: "column",
-        flexGrow: "flexGrow",
-        height: "85vh"
-    }
-    return (
-        <Box sx={wrapperStyle}>
-            {messageArray.map((item, index) => {
-                const user: IUser = users.find((user) => user.uid === item.author.uid)!;
+	if (!messageArray || !users) return <Box className={classes.wrapper} />;
 
-                return (
-                    <Message
-                        isScrollTo={index === messageArray.length - 1}
-                        key={item.uid}
-                        author={user}
-                        message={item}
-                        isAuthor={uid === item.author.uid}
-                    />
-                );
-            })}
-        </Box>
-    );
+	return (
+		<Box className={classes.wrapper}>
+			{messageArray.map((item, index) => {
+				const user: IUser = users.find((user) => user.uid === item.author.uid)!;
+
+				return (
+					<Message
+						isScrollTo={index === messageArray.length - 1}
+						key={item.uid}
+						author={user}
+						message={item}
+						isAuthor={uid === item.author.uid}
+					/>
+				);
+			})}
+		</Box>
+	);
 };
 
 export default MessageList;
