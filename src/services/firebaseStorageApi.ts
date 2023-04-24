ed2dd5d3c0015261c23f2457e6ff4e534e33ba1a
Artem Bazyl <a4b11s@gmail.com>
2023-04-25 00:36:36 +0300

    feat: add uploadFileArray and getFilesUrls

--- src/services/firebaseStorageApi.ts
+++ src/services/firebaseStorageApi.ts
@@ -10,7 +10,41 @@ export const uploadFile = (
 	return uploadBytes(storageRef, file);
 };
 
+export const uploadFileArray = (
+	path: string,
+	fileArray: Blob[] | Uint8Array[] | ArrayBuffer[]
+): Promise<string[]> => {
+	const file = fileArray.shift();
+	const uid = Date.now();
+
+	if (!file) {
+		return new Promise((resolve) => {
+			resolve([]);
+		});
+	}
+
+	return uploadFile(path + uid, file).then(async (result) => {
+		return [...(await uploadFileArray(path, fileArray)), result.ref.fullPath];
+	});
+};
+
 export const getFileUrl = (path: string) => {
 	const storageRef = ref(storage, path);
 	return getDownloadURL(storageRef);
+};
+
+export const getFilesUrls = (pathArray: string[]): Promise<string[]> => {
+	const path = pathArray.shift();
+
+	if (!path) {
+		return new Promise((resolve) => {
+			resolve([]);
+		});
+	}
+
+	const storageRef = ref(storage, path);
+
+	return getDownloadURL(storageRef).then(async (url) => {
+		return [...(await getFilesUrls(pathArray)), url];
+	});
 };
